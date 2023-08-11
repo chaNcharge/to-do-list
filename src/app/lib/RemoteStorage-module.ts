adb9b4c7e40c98aa94c40d31d5885874313f4b32
chaNcharge <chancharge@gmail.com>
2023-08-10 17:08:46 -0700

    Fix typing, convert to async function

--- src/app/lib/RemoteStorage-module.ts
+++ src/app/lib/RemoteStorage-module.ts
@@ -1,22 +1,22 @@
-export const Todos = { name: 'todos', builder: function (privateClient, publicClient) {
-    privateClient.declareType("TodoData", {
-        type: 'object',
-        properties: {
-            todosData: {
-                type: 'array',
+export const Todos = {
+    name: 'todos', builder: function (privateClient: { declareType: (arg0: string, arg1: { type: string; properties: { todosData: { type: string; }; }; required: string[]; }) => void; storeObject: (arg0: string, arg1: string, arg2: any) => Promise<any>; }, publicClient: any) {
+        privateClient.declareType("TodoData", {
+            type: 'object',
+            properties: {
+                todosData: {
+                    type: 'array',
+                },
             },
-        },
-        required: ['todosData']
-    });
+            required: ['todosData']
+        });
 
-    return {
-        exports: {
-            saveToRemoteStorage: function (data) {
-                return privateClient.storeObject("TodoData", "todos/", data)
-                    .then(function() {
-                        console.log("saved to remoteStorage");
-                    })
+        return {
+            exports: {
+                saveToRemoteStorage: async function (data: any) {
+                    await privateClient.storeObject("TodoData", "todos/", data);
+                    console.log("saved to remoteStorage");
+                }
             }
         }
     }
-}}
+}
