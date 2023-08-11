cda335bdc503e21ae8cf2e3653dd62a89317777e
chaNcharge <chancharge@gmail.com>
2023-08-10 17:17:33 -0700

    Fix one more typing issue

--- src/app/lib/RemoteStorage.ts
+++ src/app/lib/RemoteStorage.ts
@@ -2,7 +2,7 @@ import RemoteStorage from "remotestoragejs";
 import { Todo } from "../page";
 
 
-export function saveToRemoteStorage(data: Todo[], remoteStorage: RemoteStorage) {
+export function saveToRemoteStorage(data: Todo[], remoteStorage: RemoteStorage & { todos: { saveToRemoteStorage: (data: any) => Promise<void> } }) {
     remoteStorage.todos.saveToRemoteStorage({todosData: data});
 }
 
